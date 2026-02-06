bca7de45be18010d83ee685aa48b85764060f32e
M Vignesh <vigneshms0411@gmail.com>
2026-02-06 09:40:59 +0000

    Add outputs for VPC, ALB, RDS, and web servers

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,19 @@
+output "vpc_id" {
+  value = aws_vpc.streamline_vpc.id
+}
 
+output "alb_dns_name" {
+  value = aws_lb.app_lb.dns_name
+}
+
+output "rds_endpoint" {
+  value = aws_db_instance.streamline_db.endpoint
+}
+
+output "web_server_1_ip" {
+  value = aws_instance.web_server_1.public_ip
+}
+
+output "web_server_2_ip" {
+  value = aws_instance.web_server_2.public_ip
+}
