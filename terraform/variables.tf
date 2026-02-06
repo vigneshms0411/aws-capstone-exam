6629ac1a26206d08632f550df093fa35dde938d9
M Vignesh <vigneshms0411@gmail.com>
2026-02-06 09:39:36 +0000

    Define Terraform variables for AWS and database
    Added variables for AWS region, VPC CIDR, database credentials, and SSH access.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,23 @@
 
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "db_username" {
+  default = "admin"
+  sensitive = true
+}
+
+variable "db_password" {
+  default = "StreamLine2024!"
+  sensitive = true
+}
+
+variable "your_ip" {
+  description = "Your IP address for SSH access"
+  default     = "0.0.0.0/0"  # Change to your actual IP/32
+}
